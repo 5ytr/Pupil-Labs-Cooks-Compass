25415421d8d109d2a4bd7ef7f48e527a3e65b642
Alice Zhong <azhong377@gmail.com>
2025-05-09 11:42:04 -0700

    salt spawner script

---
+++ Assets/Blender Assets/SaltPlayer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] ParticleSystem saltSpawner = null;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+    }
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
