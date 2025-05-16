1b8dde7866de36c7030def6201be79acfb7db7dd
Alice Zhong <azhong377@gmail.com>
2025-05-16 13:00:54 -0700

    salt shaker toggle w space bar

--- Assets/Blender Assets/SaltPlayer.cs
+++ Assets/Blender Assets/SaltPlayer.cs
@@ -6,12 +6,20 @@ public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField] ParticleSystem saltSpawner = null;
 
+    // TODO: delete after finished testing
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            
+            Collect();
         }
+    }
+
+    public void Collect()
+    {
+        // play the collect graphics
+        saltSpawner.Play();
+        // play the collect sound effects
     }
     // // Start is called before the first frame update
     // void Start()
